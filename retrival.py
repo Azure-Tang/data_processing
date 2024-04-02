f52ac77a8e105585627f1cf7cd87729c4a897ee8
Azure <azuretang@foxmail.com>
2024-04-02 13:44:47 +0000

    modify

--- retrival.py
+++ retrival.py
@@ -73,7 +73,7 @@ def read_lookup_table(file_path):
         position_idx_record+=[idx]*len(token_string)
     return lookup_table, cat_text, position_idx_record
 
-def find_text_indices(text, lookup_table, cat_text, position_idx_record, n=20):
+def find_text_indices(text, cat_text, position_idx_record, n=20):
     # 取巧只匹配段落的前20个和后20个字符来定位整段的位置
     # 初始化 start_idx 和 end_idx 为 None
     start_idx = end_idx = None
@@ -149,13 +149,29 @@ def cal_colbert_score(q_reps, p_reps, q_mask: torch.Tensor, temperature=1.0):
     return scores
 
 class retrival_model:
-    def __init__(self,tokenizer_path:Path='../internlm2-7B', bgem3_list_file_path:str='./bgem3_output/3body.pkl', model_name: str='BAAI/bge-m3'):
-        self.model = BGEM3FlagModel(model_name, use_fp16=True)
+    def __init__(self,tokenizer_path:Path='../internlm2-7B', bgem3_list_file_path:str='./bgem3_output/3body.txt.pkl', model_name: str='BAAI/bge-m3', use_dense_and_lexical_score=True, use_colbert_score=True, use_fp16=True, book_dir:Path='./book_dir', middle_files_dir:Path='./datas'):
+        self.model = BGEM3FlagModel(model_name, use_fp16=use_fp16)
         self.splitter = ChineseRecursiveTextSplitter()
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, trust_remote_code=True)
+        if not use_dense_and_lexical_score and not use_colbert_score:
+            raise ValueError("At least one of use_dense_and_lexical_score and use_colbert_score should be True")
+        self.use_dense_and_lexical_score = use_dense_and_lexical_score
+        self.use_colbert_score = use_colbert_score
+        
+
+        if not os.path.exists(book_dir):
+            raise FileNotFoundError(f"File {book_dir} not found")
+        self.book_dir = book_dir
+        if not os.path.exists(middle_files_dir):
+            raise FileNotFoundError(f"File {middle_files_dir} not found")
+        self.middle_files_dir = middle_files_dir
+        
+
+        if not os.path.exists(bgem3_list_file_path):
+            raise FileNotFoundError(f"File {bgem3_list_file_path} not found")
         with open(bgem3_list_file_path, "rb") as file:
             self.bgem3_list = pickle.load(file)
-        
+
         self.template={
             "content": "text",
             "start": 0,
@@ -165,14 +181,13 @@ class retrival_model:
             "colbert_vecs": "matrix"
         }
 
-    def preprocess_data(self, chunk_size=250, chunk_overlap=0, data_input_dir:Path='./input_dir', output_path:Path='./bgem3_output/', tokenized_text_path:Path='./output_datas/tokenized_prompt/'):
+    def preprocess_data(self, chunk_size=250, chunk_overlap=0, book_dir:Path='./input_dir', output_path:Path='./datas/bgem3_output/', middle_files_dir:Path='./datas/tokenized_prompt/'):
         # preprocessing data, split text into chunks and encode them
-        if not os.path.notexists(data_input_dir):
-            # 
-            raise FileNotFoundError(f"File {data_input_dir} not found")
-        if not os.path.exists(tokenized_text_path):
+        if not os.path.notexists(book_dir):
+            raise FileNotFoundError(f"File {book_dir} not found")
+        if not os.path.exists(middle_files_dir):
             # tokenize text
-            self.tokenize_file(data_input_dir, tokenized_text_path)
+            self.tokenize_file(book_dir, middle_files_dir)
         if not os.path.exists(output_path):
             os.makedirs(output_path)
         
@@ -184,19 +199,19 @@ class retrival_model:
         )
 
         # walk through all files in input_data
-        for root, _, files in os.walk(data_input_dir):
+        for root, _, files in os.walk(book_dir):
             for file in files:
                 if file.endswith('.txt'):
                     with open(Path(root) / file, 'r', encoding='utf-8') as f:
                         text = f.read()
                         # split text into chunks
-                        look_up_table, cat_text, position_idx_record = read_lookup_table(tokenized_text_path/file)
+                        _, cat_text, position_idx_record = read_lookup_table(middle_files_dir/file)
                         processed_chunk_list = []
                         not_found_list = []
                         chunks = text_splitter.split_text(text)
                         for i, chunk in enumerate(chunks):
                             meta = self.template.copy()
-                            start, end = find_text_indices(chunk, look_up_table, cat_text, position_idx_record)
+                            start, end = find_text_indices(chunk, cat_text, position_idx_record)
                             if end == None:
                                 not_found_list.append(chunk)
                                 continue
@@ -207,7 +222,7 @@ class retrival_model:
                             meta['dense_vecs'] = output['dense_vecs']
                             meta['sparse_vecs'] = output['sparse_vecs']
                             meta['colbert_vecs'] = output['colbert_vecs']
-                            meta['doc_id'] = file
+                            meta['doc'] = file
                             processed_chunk_list.append(meta)
                     # save to pickle
                     # exclude .txt tail
@@ -217,32 +232,56 @@ class retrival_model:
                     with open(output_path / 'not_found.txt', 'w', encoding='utf-8') as f:
                         for chunk in not_found_list:
                             f.write(chunk + '\n')
-        
 
-    def retrive_data(self, queries, top_k=5, expand_length=0, is_merge_overlaped=False, mode='colbert+lexical_weights+dense'):
+    def expand_text(self, queries_meta_list:list[dict], expand_length=0):
+        expanded_queries_meta_list = []
+        for query_meta_list in queries_meta_list:
+            expanded_query_meta_list = []
+            for sentence_meta in query_meta_list:
+                expanded_text, _ = find_and_expand(sentence_meta['content'], self.book_dir, expansion_length=expand_length)
+                if expanded_text is None:
+                    print(f"Failed to expand text: {sentence_meta['content']}")
+                    expanded_query_meta_list.append(sentence_meta)
+                    continue
+                _, cat_text, position_idx_record = read_lookup_table(self.middle_files_dir/sentence_meta['doc'])
+                start, end = find_text_indices(expanded_text, cat_text, position_idx_record)
+                output = self.model.encode(expanded_text, return_dense=True, return_sparse=True, return_colbert_vecs=True)
+                expended_sentence_meta = {}
+                expended_sentence_meta['content'] = expanded_text
+                expended_sentence_meta['start'] = start
+                expended_sentence_meta['end'] = end
+                expended_sentence_meta['doc'] = sentence_meta['doc']
+                expended_sentence_meta['query'] = sentence_meta['query']
+            expanded_queries_meta_list.append(expanded_query_meta_list)
+        return expanded_queries_meta_list
+
+    def retrive_data(self, queries, top_k=5, expand_length=0, is_merge_overlaped=False):
         # 对queries进行检索
         score_list = self.compute_score(queries)
-        queries_top_k_idx = self.get_top_k_idx(score_list, top_k, mode)
+        queries_top_k_idx = self.get_top_k_idx(score_list, top_k)
         queries_meta_list = []
         for i, top_k_idx_list in enumerate(queries_top_k_idx):
             query_meta_list = []
             for idx in top_k_idx_list:
-                meta = {}
-                meta['content'] = self.bgem3_list[idx]['content']
-                meta['start'] = self.bgem3_list[idx]['start']
-                meta['end'] = self.bgem3_list[idx]['end']
-                # meta['doc_id'] = self.bgem3_list[idx]['doc_id']
-                meta['query'] = queries[i]
-                query_meta_list.append(meta)
+                sentence_meta = {}
+                sentence_meta['content'] = self.bgem3_list[idx]['content']
+                sentence_meta['start'] = self.bgem3_list[idx]['start']
+                sentence_meta['end'] = self.bgem3_list[idx]['end']
+                sentence_meta['doc'] = self.bgem3_list[idx]['doc']
+                sentence_meta['query'] = queries[i]
+                query_meta_list.append(sentence_meta)
             queries_meta_list.append(query_meta_list)
+        if expand_length != 0:
+            queries_meta_list = self.expand_text(queries_meta_list, expand_length)
+
         return queries_meta_list
     
-    def get_top_k_idx(self, query_score_list, k=5, mode='colbert+lexical_weights+dense'):
+    def get_top_k_idx(self, query_score_list, k=5):
         # 对每个query进行embedding的打分计算
         query_score_top_k_idx = []
         for query_score in query_score_list:
             query_score_top_k_idx.append(
-                sorted(range(len(query_score)), key=lambda i: query_score[i][mode], reverse=True)[:k]
+                sorted(range(len(query_score)), key=lambda i: query_score[i], reverse=True)[:k]
             )
         return query_score_top_k_idx
 
@@ -254,23 +293,32 @@ class retrival_model:
             query_output = self.model.encode(query, return_dense=True, return_sparse=True, return_colbert_vecs=True)
 
             for sentence in self.bgem3_list:
-                dense_score = query_output['dense_vecs']@ sentence['dense_vecs'].T
-                sparse_score = self.model.compute_lexical_matching_score(query_output['lexical_weights'], sentence['lexical_weights'])
-                colbert_score = self.model.colbert_score(query_output['colbert_vecs'], sentence['colbert_vecs'])
+                if self.use_dense_and_lexical_score:
+                    dense_score = query_output['dense_vecs']@ sentence['dense_vecs'].T
+                    lexical_weights_score = self.model.compute_lexical_matching_score(query_output['lexical_weights'], sentence['lexical_weights'])
+                if self.use_colbert_score:
+                    colbert_score = self.model.colbert_score(query_output['colbert_vecs'], sentence['colbert_vecs'])
+                
                 if weights_for_different_modes is None:
                     weights_for_different_modes = [1, 1., 1.]
-                    weight_sum = 3
-                    print("default weights for dense, lexical_weights, colbert are [1.0, 1.0, 1.0] ")
+                    weight_sum = 0
+                    if self.use_dense_and_lexical_score == True:
+                        weight_sum += 2
+                    if self.use_colbert_score == True:
+                        weight_sum += 1
+                    print("default weights for dense, lexical_weights, colbert are [1.0, 1.0, 1.0] or [1.0, 1.0] ")
                 else:
-                    assert len(weights_for_different_modes) == 3
                     weight_sum = sum(weights_for_different_modes)
                 
-                all_scores = {}
-                all_scores['dense'] = dense_score
-                all_scores['lexical_weights'] = sparse_score
-                all_scores['colbert'] = colbert_score
-                all_scores['lexical_weights+dense'] = (sparse_score * weights_for_different_modes[1] + dense_score * weights_for_different_modes[0])/(weights_for_different_modes[1]+weights_for_different_modes[0])
-                all_scores['colbert+lexical_weights+dense']= (sparse_score * weights_for_different_modes[1] + dense_score * weights_for_different_modes[0] + colbert_score * weights_for_different_modes[2])/weight_sum
-                score_list.append(all_scores)
+                all_score = 0
+                if self.use_dense_and_lexical_score:
+                    all_score += dense_score * weights_for_different_modes[0] + lexical_weights_score * weights_for_different_modes[1]
+
+                if self.use_colbert_score:
+                    all_score += colbert_score * weights_for_different_modes[2]
+
+                all_score = all_score/weight_sum 
+
+                score_list.append(all_score)
             query_score_list.append(score_list)
         return query_score_list
